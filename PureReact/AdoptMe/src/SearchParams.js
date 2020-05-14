8da382f593d42111d9b999f4bb9515338e1b25ac
JenniferGaupel <jennifergaupel@gmail.com>
2020-05-14 18:41:11 -0400

    Added dropdown for animal breed option

--- PureReact/AdoptMe/src/SearchParams.js
+++ PureReact/AdoptMe/src/SearchParams.js
@@ -4,6 +4,8 @@ import { ANIMALS } from "@frontendmasters/pet";
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -25,10 +27,29 @@ const SearchParams = () => {
             onChange={e => setAnimal(e.target.value)}
             onBlur={e => setAnimal(e.target.value)}
           >
-          <option>All</option>
-          {ANIMALS.map(animal => (
-            <option value={animal}>{animal}</option>
-          ))}
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map(breedString => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
           </select>
         </label>
         <button>Submit</button>
